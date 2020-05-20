b7cd8b3ae1f3bfa30a61166650e3d738bfd05f42
apoorv <apoorvkhare007@gmail.com>
2020-05-20 22:48:28 +0530

    Updates

---
+++ precache-manifest.41e16ebdfe3e38151a45e24a98d17848.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522cb080cc55c26f0993b9f44af38ac2",
+    "url": "/apoorv.dev/index.html"
+  },
+  {
+    "revision": "736da9e658202d9ecef9",
+    "url": "/apoorv.dev/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1399b01d95a95c3753a6",
+    "url": "/apoorv.dev/static/css/main.5f2b8e7c.chunk.css"
+  },
+  {
+    "revision": "736da9e658202d9ecef9",
+    "url": "/apoorv.dev/static/js/2.92cdeb06.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/apoorv.dev/static/js/2.92cdeb06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1399b01d95a95c3753a6",
+    "url": "/apoorv.dev/static/js/main.19fa3659.chunk.js"
+  },
+  {
+    "revision": "05cc0fd222df674ca7b7",
+    "url": "/apoorv.dev/static/js/runtime-main.3d5e9e1b.js"
+  },
+  {
+    "revision": "7d563114074bfca5f3beef4a260cdd7d",
+    "url": "/apoorv.dev/static/media/logo.7d563114.png"
+  },
+  {
+    "revision": "377604d2d91a778c561782ae8f4acb88",
+    "url": "/apoorv.dev/static/media/me.377604d2.jpg"
+  }
+]);
