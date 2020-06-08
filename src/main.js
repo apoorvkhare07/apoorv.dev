d08610330693d09d5219a2e63a2ac5f712d46d03
apoorv <apoorvkhare007@gmail.com>
2020-06-08 18:03:32 +0530

    cv path fix

--- src/main.js
+++ src/main.js
@@ -37,17 +37,18 @@ class Main extends Component{
                         <p> I'm a software developer working towards building <span style={{color: "#ffd479"}}>cross‑platform </span> and <span style={{color: "#ffd479"}}>full‑stack </span> web and mobile apps, <span style={{color: "#ffd479"}}> opensource </span> projects and <span style={{color: "#ffd479"}}> writing </span> about new technologies.</p>
                         <p className = "call-to-act">
                         <a href={Pdf} target="_blank"><button className="email-cta-2">View CV</button></a>
-                        <a><button className="email-cta">Email me</button></a>
+                        <button onclick="location.href='mailto:apoorvkhare007@gmail.com" className="email-cta">Email me</button>
                         </p>
                     </Col>
 
                     <Col md = {4} lg = {4} className="elevator">
                         <img src = {me} className="my-image" />
                         <p>
-                            I’m currently looking for internship in software development. If you have interesting project for me, let’s talk.
+                            I’m currently looking for internship in software development. Get in touch if you have an interesting project to discuss. I'm around.
+
                         </p>
                         <a href={Pdf} target="_blank"><button className="email-cta-2">View CV</button></a>
-                        <a><button className="email-cta">Email me</button></a>
+                        <a target = "_blank" href="mailto:apoorvkhare007@gmail.com" ><button className="email-cta">Email me</button></a>
 
 
                     </Col>
