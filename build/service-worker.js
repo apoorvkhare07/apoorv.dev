f3a3f70ef9ee057a3fb2ec3b611352860fbaf30f
apoorv <apoorvkhare007@gmail.com>
2020-06-08 18:38:08 +0530

    update build command

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e655a2c3a32586dcedaf251007119223.js"
+  "/precache-manifest.374d0f4277aed9690710190ee2fa4b23.js"
 );
 
 self.addEventListener('message', (event) => {
